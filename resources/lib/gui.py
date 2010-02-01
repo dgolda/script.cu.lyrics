77277f55af015fff4e285492926b28a5577ef730
amet1977 <amet1977@c04601c8-0a8a-11df-beaf-bb271b2086c6>
2010-02-01 07:23:57 +0000

    Removed lyricsflyScraper, we couldnt get the permanent API key :(

--- resources/lib/gui.py
+++ resources/lib/gui.py
@@ -52,14 +52,7 @@ class GUI( xbmcgui.WindowXMLDialog ):
         self.settings = Settings().get_settings()
         
     def get_scraper( self ):
-        if int(__settings__.getSetting( "scraper" )) == 0:
-          import lyricsflyScraper as lyricsScraper
-          print "Scraper: lyricsfly.com"
-        else:
-          import lyricsScraper as lyricsScraper
-          print "Scraper: lyricsWiki.org"
-
-        
+        import lyricsScraper as lyricsScraper        
         self.LyricsScraper = lyricsScraper.LyricsFetcher()
         self.scraper_title = lyricsScraper.__title__
         self.scraper_exceptions = lyricsScraper.__allow_exceptions__
