db5da9a444387f98a769c75b4cf1ab14666d31cf
Zeljko Ametovic <amet@no.spam>
2010-12-15 13:15:50 +0400

    added two new scrapers, lyricstime and lyricsmode

--- script.cu.lyrics/default.py
+++ script.cu.lyrics/default.py
@@ -9,21 +9,23 @@ __author__ = "Amet, ZorMonkey"
 __url__ = ""
 __scriptid__ = "script.cu.lyrics"
 __credits__ = "EnderW,Nuka1195"
-__version__ = "0.9.2"
+__version__ = "0.9.9"
 __XBMC_Revision__ = "30001"
 
+__settings__   = xbmcaddon.Addon(id=__scriptid__)
+__language__   = __settings__.getLocalizedString
+__version__    = __settings__.getAddonInfo('version')
+__cwd__        = __settings__.getAddonInfo('path')
+
 # Shared resources 
-BASE_RESOURCE_PATH = xbmc.translatePath( os.path.join( os.getcwd(), 'resources', 'lib' ) )
+BASE_RESOURCE_PATH = xbmc.translatePath( os.path.join( __cwd__, 'resources', 'lib' ) )
 
 sys.path.append (BASE_RESOURCE_PATH)
-
-__settings__ = xbmcaddon.Addon(id=__scriptid__)
-__language__ = __settings__.getLocalizedString
 
 if ( __name__ == "__main__" ):
     import gui as gui
     window = "main"
-    ui = gui.GUI( "script-XBMC_Lyrics-main.xml" , os.getcwd(), "Default" )
+    ui = gui.GUI( "script-XBMC_Lyrics-main.xml" , __cwd__, "Default" )
     ui.doModal()
     del ui
     sys.modules.clear()
